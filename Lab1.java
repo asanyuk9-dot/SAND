ff5edc8fbb34c8569cf66890edbc3248c9c30d5b
asanyuk9-dot <asanyuk9@gmail.com>
2025-10-08 14:09:21 +0000

    System.out.println("Hell, World!");     System.out.println("Fire!");

--- Lab1.java
+++ Lab1.java
@@ -2,7 +2,7 @@
 
 public static void main(String[] args) 
 {
-    System.out.println("Hello, World!");
-    System.out.println("Hell");
+    System.out.println("Hell, World!");
+    System.out.println("Fire!");
     }
 }
